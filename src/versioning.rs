91e27b6204d12e755aa2d376fecd1d2a56443ca5
TomascpMarques <tomascpmarques@gmail.com>
2022-07-19 22:33:51 +0100

    code fixes

--- src/versioning.rs
+++ src/versioning.rs
@@ -1,7 +1,13 @@
 use std::fmt::Display;
 
 #[derive(Default, Debug, PartialEq, Eq, Hash, Clone)]
-pub struct SemanticVersion(pub u32, pub u32, pub u32);
+pub struct SemanticVersion(pub String, pub String, pub String);
+
+impl Display for SemanticVersion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}.{}.{}", self.0, self.1, self.2)
+    }
+}
 
 impl SemanticVersion {
     /// Creates a new Semantic version from a string separated by dots
@@ -15,9 +21,9 @@ impl SemanticVersion {
     ///   let have = SemanticVersion::new("1.3.42").unwrap();
     ///   let (mj, mn, pt) = (have.major(), have.minor(), have.patch());
     ///
-    ///   assert_eq!(mj, 1);
-    ///   assert_eq!(mn, 3);
-    ///   assert_eq!(pt, 42)
+    ///   assert_eq!(mj, "1".to_string());
+    ///   assert_eq!(mn, "3".to_string());
+    ///   assert_eq!(pt, "42".to_string())
     /// # }
     /// ```
     pub fn new<T>(target: T) -> Result<Self, String>
@@ -28,14 +34,19 @@ impl SemanticVersion {
             .to_string()
             .split('.')
             .take(3)
-            .filter_map(|x| x.parse().ok())
-            .collect::<Vec<u32>>();
+            .filter_map(|x| x.parse::<u32>().ok())
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>();
 
         if formatted.len().ne(&3) {
             return Err("Bad value was given".to_owned());
         }
 
-        Ok(Self(formatted[0], formatted[1], formatted[2]))
+        Ok(Self(
+            formatted[0].clone(),
+            formatted[1].clone(),
+            formatted[2].clone(),
+        ))
     }
 
     pub fn is_semantic_v<T>(target: T) -> bool
@@ -46,19 +57,19 @@ impl SemanticVersion {
     }
 
     pub fn new_from_numbers(num: u32, num1: u32, num2: u32) -> Self {
-        Self(num, num1, num2)
+        Self(num.to_string(), num1.to_string(), num2.to_string())
     }
 
-    pub fn major(&self) -> u32 {
-        self.0
+    pub fn major(&self) -> String {
+        self.0.clone()
     }
 
-    pub fn minor(&self) -> u32 {
-        self.1
+    pub fn minor(&self) -> String {
+        self.1.clone()
     }
 
-    pub fn patch(&self) -> u32 {
-        self.2
+    pub fn patch(&self) -> String {
+        self.2.clone()
     }
 }
 
@@ -71,9 +82,9 @@ mod test_semantic_versioning_suport {
         let have = SemanticVersion::new("1.3.42").unwrap();
         let (mj, mn, pt) = (have.major(), have.minor(), have.patch());
 
-        assert_eq!(mj, 1);
-        assert_eq!(mn, 3);
-        assert_eq!(pt, 42)
+        assert_eq!(mj, "1".to_string());
+        assert_eq!(mn, "3".to_string());
+        assert_eq!(pt, "42".to_string())
     }
 
     #[test]
@@ -92,9 +103,9 @@ mod test_semantic_versioning_suport {
     fn test_semantic_version_zero_padded() {
         match SemanticVersion::new("03.04.0123") {
             Ok(v) => {
-                assert_eq!(v.0, 3);
-                assert_eq!(v.1, 4);
-                assert_eq!(v.2, 123)
+                assert_eq!(v.0, "3".to_string());
+                assert_eq!(v.1, "4".to_string());
+                assert_eq!(v.2, "123".to_string())
             }
             Err(_) => assert!(true),
         }
